0b0ff46bdb0b26dd5b79107d1983fe501bc4be76
Veda Sheersh Boorla <mailforveda@gmail.com>
2023-12-19 02:56:37 -0500

    Update data.py

--- tf_bind_transformer/data.py
+++ tf_bind_transformer/data.py
@@ -18,7 +18,6 @@ from tf_bind_transformer.gene_utils import parse_gene_name
 from enformer_pytorch import FastaInterval
 
 from pyfaidx import Fasta
-import pybedtools
 
 def exists(val):
     return val is not None
@@ -286,481 +285,73 @@ def generate_random_ranges_from_fasta(
 
 # context string creator class
 
-class ContextDataset(Dataset):
+class ProteinSmilesDataset(Dataset):
     def __init__(
         self,
         *,
-        biotypes_metadata_path = None,
-        include_biotypes_metadata_in_context = False,
-        include_biotypes_metadata_columns = [],
-        biotypes_metadata_delimiter = ' | ',
-    ):
-        self.include_biotypes_metadata_in_context = include_biotypes_metadata_in_context
-        self.include_biotypes_metadata_columns = include_biotypes_metadata_columns
-        self.biotypes_metadata_delimiter = biotypes_metadata_delimiter
-
-        if include_biotypes_metadata_in_context:
-            assert len(self.include_biotypes_metadata_columns) > 0, 'must have more than one biotype metadata column to include'
-            assert exists(biotypes_metadata_path), 'biotypes metadata path must be supplied if to be included in context string'
-
-            p = Path(biotypes_metadata_path)
-
-            if p.suffix == '.csv':
-                sep = ','
-            elif p.suffix == '.tsv':
-                sep = '\t'
-            else:
-                raise ValueError(f'invalid suffix {p.suffix} for biotypes')
-
-            self.df = pl.read_csv(str(p), sep = sep)
-
-    def __len__():
-        return len(self.df) if self.include_biotypes_metadata_in_context else -1
-
-    def __getitem__(self, biotype):
-        if not self.include_biotypes_metadata_in_context:
-            return biotype
-
-        col_indices = list(map(self.df.columns.index, self.include_biotypes_metadata_columns))
-        filtered = self.df.filter(pl.col('biotype') == biotype)
-
-        if len(filtered) == 0:
-            print(f'no rows found for {biotype} in biotype metadata file')
-            return biotype
-
-        row = filtered.row(0)
-        columns = list(map(lambda t: row[t], col_indices))
-
-        context_string = self.biotypes_metadata_delimiter.join([biotype, *columns])
-        return context_string
-
-# dataset for remap data - all peaks
-
-class RemapAllPeakDataset(Dataset):
-    def __init__(
-        self,
-        *,
-        factor_fasta_folder,
-        bed_file = None,
-        remap_df = None,
-        filter_chromosome_ids = None,
+        data_folder,
+        csv_file = None,
+        df = None,
         exclude_targets = None,
         include_targets = None,
-        exclude_cell_types = None,
-        include_cell_types = None,
-        remap_df_frac = 1.,
+        df_frac = 1.,
         experiments_json_path = None,
-        include_biotypes_metadata_in_context = False,
-        biotypes_metadata_path = None,
-        include_biotypes_metadata_columns = [],
-        biotypes_metadata_delimiter = ' | ',
-        balance_sampling_by_target = False,
+        balance_sampling_by_target_bin = False,
         **kwargs
     ):
         super().__init__()
-        assert exists(remap_df) ^ exists(bed_file), 'either remap bed file or remap dataframe must be passed in'
+        assert exists(csv_file) ^ exists(df), 'either data csv file or dataframe must be passed in'
 
-        if not exists(remap_df):
-            remap_df = read_bed(bed_file)
+        if not exists(df):
+            df = read_bed(csv_file)
 
-        if remap_df_frac < 1:
-            remap_df = remap_df.sample(frac = remap_df_frac)
+        if df_frac < 1:
+            df_frac = df.sample(frac = df_frac)
 
-        dataset_chr_ids = CHR_IDS
-
-        if exists(filter_chromosome_ids):
-            dataset_chr_ids = dataset_chr_ids.intersection(set(filter_chromosome_ids))
-
-        remap_df = remap_df.filter(pl_isin('column_1', get_chr_names(dataset_chr_ids)))
-        remap_df = filter_df_by_tfactor_fastas(remap_df, factor_fasta_folder)
-
-        self.factor_ds = FactorProteinDataset(factor_fasta_folder)
-
-        # filter dataset by inclusion and exclusion list of targets
-        # (<all available targets> intersect <include targets>) subtract <exclude targets>
-
-        include_targets = cast_list(include_targets)
-        exclude_targets = cast_list(exclude_targets)
-
-        if include_targets:
-            remap_df = remap_df.filter(pl_isin('target', include_targets))
-
-        if exclude_targets:
-            remap_df = remap_df.filter(pl_notin('target', exclude_targets))
-
-        # filter dataset by inclusion and exclusion list of cell types
-        # same logic as for targets
-
-        include_cell_types = cast_list(include_cell_types)
-        exclude_cell_types = cast_list(exclude_cell_types)
-
-        if include_cell_types:
-            remap_df = remap_df.filter(pl_isin('cell_type', include_cell_types))
-
-        if exclude_cell_types:
-            remap_df = remap_df.filter(pl_notin('cell_type', exclude_cell_types))
-
-        assert len(remap_df) > 0, 'dataset is empty by filter criteria'
-
-        self.df = remap_df
-        self.fasta = FastaInterval(**kwargs)
+        self.df = df
 
         self.experiments_index = fetch_experiments_index(experiments_json_path)
 
         # balanced target sampling logic
 
-        self.balance_sampling_by_target = balance_sampling_by_target
+        self.balance_sampling_by_target_bin = balance_sampling_by_target_bin
 
-        if self.balance_sampling_by_target:
-            self.df_indexed_by_target = []
+        if self.balance_sampling_by_target_bin:
+            self.df_indexed_by_target_bin = []
 
-            for target in self.df.get_column('target').unique().to_list():
-                df_by_target = self.df.filter(pl.col('target') == target)
-                self.df_indexed_by_target.append(df_by_target)
-
-        # context string creator
-
-        self.context_ds = ContextDataset(
-            include_biotypes_metadata_in_context = include_biotypes_metadata_in_context,
-            biotypes_metadata_path = biotypes_metadata_path,
-            include_biotypes_metadata_columns = include_biotypes_metadata_columns,
-            biotypes_metadata_delimiter = biotypes_metadata_delimiter
-        )
+            for target in self.df.get_column('target_bin').unique().to_list():
+                df_by_target_bin = self.df.filter(pl.col('target') == target)
+                self.df_indexed_by_target_bin.append(df_by_target)
 
     def __len__(self):
-        if self.balance_sampling_by_target:
-            return len(self.df_indexed_by_target)
+        if self.balance_sampling_by_target_bin:
+            return len(self.df_indexed_by_target_bin)
         else:
             return len(self.df)
 
     def __getitem__(self, ind):
-        # if balancing by target, randomly draw sample from indexed dataframe
+        # if balancing by target bin, randomly draw sample from indexed dataframe
 
-        if self.balance_sampling_by_target:
-            filtered_df = self.df_indexed_by_target[ind]
+        if self.balance_sampling_by_target_bin:
+            filtered_df = self.df_indexed_by_target_bin[ind]
             rand_ind = randrange(0, len(filtered_df))
             sample = filtered_df.row(rand_ind)
         else:
             sample = self.df.row(ind)
 
-        chr_name, begin, end, _, _, _, experiment_target_cell_type, reading, *_ = sample
+        aa_seq, smiles, target, *_ = sample
 
         # now aggregate all the data
 
-        experiment, target, cell_type = parse_exp_target_cell(experiment_target_cell_type)
+        label = torch.Tensor([target])
 
-        seq = self.fasta(chr_name, begin, end)
-        aa_seq = self.factor_ds[target]
-        context_str = self.context_ds[cell_type]
-
-        read_value = torch.Tensor([reading])
-
-        peaks_nr = self.experiments_index.get(experiment_target_cell_type, 0.)
-        peaks_nr = torch.Tensor([peaks_nr])
-
-        label = torch.Tensor([1.])
-
-        return seq, aa_seq, context_str, peaks_nr, read_value, label
-
-# filter functions for exp-target-cells based on heldouts
-
-def filter_exp_target_cell(
-    arr,
-    *,
-    exclude_targets = None,
-    include_targets = None,
-    exclude_cell_types = None,
-    include_cell_types = None,
-):
-    out = []
-
-    for el in arr:
-        experiment, target, cell_type = parse_exp_target_cell(el)
-
-        if exists(include_targets) and len(include_targets) > 0 and target not in include_targets:
-            continue
-
-        if exists(exclude_targets) and target in exclude_targets:
-            continue
-
-        if exists(include_cell_types) and len(include_cell_types) > 0 and cell_type not in include_cell_types:
-            continue
-
-        if exists(exclude_cell_types) and cell_type in exclude_cell_types:
-            continue
-
-        out.append(el)
-
-    return out
-
-
-# dataset for negatives scoped to a specific exp-target-celltype
-
-class ScopedNegativePeakDataset(Dataset):
-    def __init__(
-        self,
-        *,
-        fasta_file,
-        factor_fasta_folder,
-        numpy_folder_with_scoped_negatives,
-        exts = '.bed.bool.npy',
-        remap_bed_file = None,
-        remap_df = None,
-        filter_chromosome_ids = None,
-        experiments_json_path = None,
-        exclude_targets = None,
-        include_targets = None,
-        exclude_cell_types = None,
-        include_cell_types = None,
-        include_biotypes_metadata_in_context = False,
-        biotypes_metadata_path = None,
-        include_biotypes_metadata_columns = [],
-        biotypes_metadata_delimiter = ' | ',
-        balance_sampling_by_target = False,
-        **kwargs
-    ):
-        super().__init__()
-        assert exists(remap_df) ^ exists(remap_bed_file), 'either remap bed file or remap dataframe must be passed in'
-
-        if not exists(remap_df):
-            remap_df = read_bed(remap_bed_file)
-
-        dataset_chr_ids = CHR_IDS
-
-        if exists(filter_chromosome_ids):
-            dataset_chr_ids = dataset_chr_ids.intersection(set(filter_chromosome_ids))
-
-        filter_map_df = remap_df.with_column(pl.when(pl_isin('column_1', get_chr_names(dataset_chr_ids))).then(True).otherwise(False).alias('mask'))
-        mask = filter_map_df.get_column('mask').to_numpy()
-
-        num_scoped_negs = mask.sum()
-
-        print(f'{num_scoped_negs} scoped negative rows found for training')
-
-        assert num_scoped_negs > 0, 'all remap rows filtered out for scoped negative peak dataset'
-
-        self.df = remap_df
-        self.chromosome_mask = mask
-
-        # get dictionary with exp-target-cell to boolean numpy indicating which ones are negatives
-
-        npys_paths = [*Path(numpy_folder_with_scoped_negatives).glob('**/*.npy')]
-        exp_target_cell_negatives = [(path.name.rstrip(exts), path) for path in npys_paths]
-
-        exp_target_cells = [el[0] for el in exp_target_cell_negatives]
-
-        exp_target_cells = filter_exp_target_cell(
-            exp_target_cells,
-            include_targets = include_targets,
-            exclude_targets = exclude_targets,
-            include_cell_types = include_cell_types,
-            exclude_cell_types = exclude_cell_types
-        )
-
-        filtered_exp_target_cell_negatives = list(filter(lambda el: el[0] in exp_target_cells, exp_target_cell_negatives))
-
-        self.exp_target_cell_negatives = filtered_exp_target_cell_negatives
-        assert len(self.exp_target_cell_negatives) > 0, 'no experiment-target-cell scoped negatives to select from after filtering'
-
-        # balanced target sampling
-
-        self.balance_sampling_by_target = balance_sampling_by_target
-
-        if balance_sampling_by_target:
-            self.exp_target_cell_by_target = defaultdict(list)
-
-            for exp_target_cell, filepath in self.exp_target_cell_negatives:
-                _, target, *_ = parse_exp_target_cell(exp_target_cell)
-                self.exp_target_cell_by_target[target].append((exp_target_cell, filepath))
-
-        # tfactor dataset
-
-        self.factor_ds = FactorProteinDataset(factor_fasta_folder)
-
-        self.fasta = FastaInterval(fasta_file = fasta_file, **kwargs)
-        self.experiments_index = fetch_experiments_index(experiments_json_path)
-
-        # context string creator
-
-        self.context_ds = ContextDataset(
-            include_biotypes_metadata_in_context = include_biotypes_metadata_in_context,
-            biotypes_metadata_path = biotypes_metadata_path,
-            include_biotypes_metadata_columns = include_biotypes_metadata_columns,
-            biotypes_metadata_delimiter = biotypes_metadata_delimiter
-        )
-
-    def __len__(self):
-        if self.balance_sampling_by_target:
-            return len(self.exp_target_cell_by_target)
-        else:
-            return len(self.exp_target_cell_negatives)
-
-    def __getitem__(self, idx):
-        if self.balance_sampling_by_target:
-            negatives = list(self.exp_target_cell_by_target.values())[idx]
-            sample = choice(negatives)
-        else:
-            sample = self.exp_target_cell_negatives[idx]
-
-        exp_target_cell, bool_numpy_path = sample
-        experiment, target, cell_type = parse_exp_target_cell(exp_target_cell)
-
-        # load boolean numpy array
-        # and select random peak that is a negative
-
-        np_arr = np.load(str(bool_numpy_path))
-        np_arr_noised = np_arr.astype(np.float32) + np.random.uniform(low = -1e-1, high = 1e-1, size = np_arr.shape[0])
-
-        # mask with chromosomes allowed
-
-        np_arr_noised *= self.chromosome_mask.astype(np.float32)
-
-        # select random negative peak
-
-        random_neg_peak_index = np_arr_noised.argmax()
-
-        chr_name, begin, end, *_ = self.df.row(random_neg_peak_index)
-        seq = self.fasta(chr_name, begin, end)
-
-        aa_seq = self.factor_ds[target]
-        context_str = self.context_ds[cell_type]
-
-        peaks_nr = self.experiments_index.get(exp_target_cell, 0.)
-        peaks_nr = torch.Tensor([peaks_nr])
-
-        read_value = torch.Tensor([0.])
-
-        label = torch.Tensor([0.])
-
-        return seq, aa_seq, context_str, peaks_nr, read_value, label
-
-# dataset for hard negatives (negatives to all peaks)
-
-class NegativePeakDataset(Dataset):
-    def __init__(
-        self,
-        *,
-        factor_fasta_folder,
-        negative_bed_file = None,
-        remap_bed_file = None,
-        remap_df = None,
-        negative_df = None,
-        filter_chromosome_ids = None,
-        exclude_targets = None,
-        include_targets = None,
-        exclude_cell_types = None,
-        include_cell_types = None,
-        exp_target_cell_column = 'column_4',
-        experiments_json_path = None,
-        include_biotypes_metadata_in_context = False,
-        biotypes_metadata_path = None,
-        include_biotypes_metadata_columns = [],
-        biotypes_metadata_delimiter = ' | ',
-        balance_sampling_by_target = False,
-        **kwargs
-    ):
-        super().__init__()
-        assert exists(remap_df) ^ exists(remap_bed_file), 'either remap bed file or remap dataframe must be passed in'
-        assert exists(negative_df) ^ exists(negative_bed_file), 'either negative bed file or negative dataframe must be passed in'
-
-        # instantiate dataframes if not passed in
-
-        if not exists(remap_df):
-            remap_df = read_bed(remap_bed_file)
-
-        neg_df = negative_df
-        if not exists(negative_df):
-            neg_df = read_bed(negative_bed_file)
-
-        # filter remap dataframe
-
-        remap_df = filter_df_by_tfactor_fastas(remap_df, factor_fasta_folder)
-
-        dataset_chr_ids = CHR_IDS
-
-        if exists(filter_chromosome_ids):
-            dataset_chr_ids = dataset_chr_ids.intersection(set(filter_chromosome_ids))
-
-        neg_df = neg_df.filter(pl_isin('column_1', get_chr_names(dataset_chr_ids)))
-
-        assert len(neg_df) > 0, 'dataset is empty by filter criteria'
-
-        self.neg_df = neg_df
-
-        # get all exp-target-cells and filter by above
-
-        exp_target_cells = remap_df.get_column(exp_target_cell_column).unique().to_list()
-
-        self.filtered_exp_target_cells = filter_exp_target_cell(
-            exp_target_cells,
-            include_targets = include_targets,
-            exclude_targets = exclude_targets,
-            include_cell_types = include_cell_types,
-            exclude_cell_types = exclude_cell_types
-        )
-
-        assert len(self.filtered_exp_target_cells), 'no experiment-target-cell left for hard negative set'
-
-        # balanced sampling of targets
-
-        self.balance_sampling_by_target = balance_sampling_by_target
-
-        if balance_sampling_by_target:
-            self.exp_target_cell_by_target = defaultdict(list)
-
-            for exp_target_cell in self.filtered_exp_target_cells:
-                _, target, *_ = parse_exp_target_cell(exp_target_cell)
-                self.exp_target_cell_by_target[target].append(exp_target_cell)
-
-        # factor ds
-
-        self.factor_ds = FactorProteinDataset(factor_fasta_folder)
-        self.fasta = FastaInterval(**kwargs)
-
-        self.experiments_index = fetch_experiments_index(experiments_json_path)
-
-        # context string creator
-
-        self.context_ds = ContextDataset(
-            include_biotypes_metadata_in_context = include_biotypes_metadata_in_context,
-            biotypes_metadata_path = biotypes_metadata_path,
-            include_biotypes_metadata_columns = include_biotypes_metadata_columns,
-            biotypes_metadata_delimiter = biotypes_metadata_delimiter
-        )
-
-    def __len__(self):
-        return len(self.neg_df)
-
-    def __getitem__(self, ind):
-        chr_name, begin, end = self.neg_df.row(ind)
-
-        if self.balance_sampling_by_target:
-            rand_ind = randrange(0, len(self.exp_target_cell_by_target))
-            exp_target_cell_by_target_list = list(self.exp_target_cell_by_target.values())
-            random_exp_target_cell_type = choice(exp_target_cell_by_target_list[rand_ind])
-        else:
-            random_exp_target_cell_type = choice(self.filtered_exp_target_cells)
-
-        experiment, target, cell_type = parse_exp_target_cell(random_exp_target_cell_type)
-
-        seq = self.fasta(chr_name, begin, end)
-        aa_seq = self.factor_ds[target]
-        context_str = self.context_ds[cell_type]
-
-        read_value = torch.Tensor([0.])
-
-        peaks_nr = self.experiments_index.get(random_exp_target_cell_type, 0.)
-        peaks_nr = torch.Tensor([peaks_nr])
-
-        label = torch.Tensor([0.])
-
-        return seq, aa_seq, context_str, peaks_nr, read_value, label
+        return smiles, aa_seq, label
 
 # dataloader related functions
 
 def collate_fn(data):
-    seq, aa_seq, context_str, peaks_nr, read_values, labels = list(zip(*data))
-    return torch.stack(seq), tuple(aa_seq), tuple(context_str), torch.stack(peaks_nr, dim = 0), torch.stack(read_values, dim = 0), torch.cat(labels, dim = 0)
+    smiles, aa_seq, labels = list(zip(*data))
+    return tuple(smiles), tuple(aa_seq), torch.cat(labels, dim = 0)
 
 def collate_dl_outputs(*dl_outputs):
     outputs = list(zip(*dl_outputs))
